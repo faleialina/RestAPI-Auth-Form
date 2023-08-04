52c2bad792efdd43a33ff718908c4dbbe71884c5
faleialina <august3@mail.ru>
2023-08-04 22:24:55 +0300

    reg

---
+++ client/app.js
@@ -0,0 +1,27 @@
+const btnReg = document.querySelector('.btn-reg');
+const inpUsernameReg = document.querySelector('.Username-inp-reg');
+const inpEmailReg = document.querySelector('.E-mail-inp-reg');
+const inpPhoneReg = document.querySelector('.Phone-inp-reg');
+const inpPasswordReg = document.querySelector('.Password-inp-reg');
+const inpConfirmPasswordReg = document.querySelector('.Confirm-Password-inp-reg');
+
+btnReg.addEventListener('click', async () => {
+    const objectToSerwer = {
+        username: inpUsernameReg.value,
+        email: inpEmailReg.value,
+        phone: inpPhoneReg.value,
+        pwd: inpPasswordReg.value,
+    };
+
+    const response = await fetch('http://localhost:3000/api/register', {
+        method: 'POST',
+        body: JSON.stringify(objectToSerwer),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    console.log(response.status);
+    console.log(await response.json());
+
+});
